bcf4ba64860d5559d4b6faadf78ab72414922512
sathish kumar <rbsathish27@gmail.com>
2020-08-24 20:56:44 +0530

    updated code img

--- src/rename_fun.py
+++ src/rename_fun.py
@@ -4,16 +4,18 @@ def rename(location,name,count):
     l =location
     n = name
     c = count
-    print("c",c)
-    print("Location of the folder : {} \n New name : {} \n Starting number : {}".format(l,n,c))
+    # print("c",c)
+    # print("Location of the folder : {} \n New name : {} \n Starting number : {}".format(l,n,c))
 
     for root, directories, files in os.walk(l, topdown=False):
         for name in files:
+            # print("old file names")
             path = os.path.join(root, name)
             file_type = path.split(".")[1]
-            print("file name",name)
-            print(path)
+            # print("file name",name)
+            print("Old file name : ",path)
             newname = '{}{}.{}'.format(n,c,file_type)
             # os.rename(filename, '{}_{}.{}'.format(n,c,file_type))
             os.rename(os.path.join(root, name), os.path.join(root, newname))
+            print("New file name : ",root,newname)
             c  += 1
